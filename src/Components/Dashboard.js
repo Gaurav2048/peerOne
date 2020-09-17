4470b5e6bd0970c858b41ae9e606ffbec553a8e1
Gunjan Kalita <gunjankalita836@gmail.com>
2020-09-17 22:21:01 +0530

    Add peerJs to app

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,27 +1,58 @@
-import React from 'react';
-import { Container, Row, Col, InputGroup, FormControl, Button } from 'react-bootstrap';
-import ChatHead from "../Components/ChatHead/ChatHead";
-import HeadSection from "../Components/Chat/HeadSection";
-import Chat from "../Components/Chat/Chat";
-
+import React, { useState } from 'react';
+import {
+  Container,
+  Row,
+  Col,
+  InputGroup,
+  FormControl,
+  Button,
+} from 'react-bootstrap';
+import ChatHead from '../Components/ChatHead/ChatHead';
+import HeadSection from '../Components/Chat/HeadSection';
+import Chat from '../Components/Chat/Chat';
+import TypeArea from '../Components/TypeArea';
 
 const Dashboard = () => {
+  const [state, setState] = useState({
+    users: [
+      {
+        id: '0',
+        name: 'Gunjan Kalita',
+        email: 'gunjan@gmail.com',
+      },
+      {
+        id: '1',
+        name: 'Other One Ka',
+        email: 'okfine@gmail.com',
+      },
+      {
+        id: '2',
+        name: 'Sara Du',
+        email: 'sarea@gmail.com',
+      },
+      {
+        id: '3',
+        name: 'Sameer Sudhi',
+        email: 'sameer@gmail.com',
+      },
+    ],
+  });
+
   return (
     <Container
       style={{
         width: '80%',
-        height: '96%',
+        height: '96vh',
         marginLeft: '10%',
         marginRight: '10%',
         border: '1px solid #cccccc',
+        borderRadius: '8px',
         marginTop: '2%',
         marginBottom: '2%',
       }}
     >
-
       <Row>
-        <Col md={4} className = "bg-light" >
-
+        <Col md={4} className="bg-light" style={{ marginTop: '8%' }}>
           <InputGroup className="mb-3" className="mt-3 mb-3">
             <FormControl
               placeholder="Search User"
@@ -32,19 +63,18 @@ const Dashboard = () => {
               <Button variant="outline-secondary">Button</Button>
             </InputGroup.Append>
           </InputGroup>
-          
-        <ChatHead />
-        <ChatHead />
-        <ChatHead />
 
+          {state.users.map((user) => (
+            <ChatHead user={user} />
+          ))}
         </Col>
 
         <Col md={8}>
           <HeadSection />
           <Chat />
+          <TypeArea />
         </Col>
       </Row>
-
     </Container>
   );
 };
