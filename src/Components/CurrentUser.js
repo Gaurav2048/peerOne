d8f5849e1401057a57f9c3d8b747c05f3814bfdc
Gunjan Kalita <gunjankalita836@gmail.com>
2020-09-17 22:39:05 +0530

    Add current user UI

---
+++ src/Components/CurrentUser.js
@@ -0,0 +1,63 @@
+import React from 'react';
+
+const CurrentUser = () => {
+  return (
+    <div
+      style={{
+        paddingTop: '7px',
+        paddingBottom: '7px',
+        backgroundColor: '#f7f7f7',
+        padding: '10px',
+        display: 'flex',
+        flexDirection: 'row',
+        borderBottom: '1px solid #dddddd ',
+        cursor: 'pointer',
+      }}
+    >
+      <div style={{ position: 'relative' }}>
+        <img
+          style={{
+            width: '60px',
+            height: '60px',
+            borderRadius: '30px',
+            objectPosition: 'center',
+            objectFit: 'cover',
+            border: '2px solid #dddddd',
+            zIndex: 0,
+          }}
+          src="https://images.pexels.com/photos/1431282/pexels-photo-1431282.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+        />
+        <div
+          style={{
+            width: '20px',
+            height: '20px',
+            backgroundColor: '#0f0',
+            border: '2px solid #dddddd',
+            borderRadius: '10px',
+            marginLeft: '40px',
+            marginTop: '-18px',
+            zIndex: 100,
+            position: 'relative',
+          }}
+        ></div>
+      </div>
+      <div style={{ marginLeft: '10px', marginTop: '4px' }}>
+        <div style={{ color: '#000', fontWeight: 'bold', fontSize: '16px' }}>
+          Gunjan K.
+        </div>
+        <div
+          style={{
+            color: '#a9a9a9',
+            fontWeight: 'normal',
+            fontSize: '14px',
+            marginTop: '-4px',
+          }}
+        >
+          gunjan@gmail.com
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CurrentUser;
