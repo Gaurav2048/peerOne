a11fe7b025f7df4d891345df54883d717483c8fa
Gaurav2048 <gunjankalita836@gmail.com>
2020-09-17 17:26:09 +0530

    Update dashboard to have ChatHead, HeadSection and Chat

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,5 +1,9 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Row, Col, InputGroup, FormControl, Button } from 'react-bootstrap';
+import ChatHead from "../Components/ChatHead/ChatHead";
+import HeadSection from "../Components/Chat/HeadSection";
+import Chat from "../Components/Chat/Chat";
+
 
 const Dashboard = () => {
   return (
@@ -13,7 +17,35 @@ const Dashboard = () => {
         marginTop: '2%',
         marginBottom: '2%',
       }}
-    ></Container>
+    >
+
+      <Row>
+        <Col md={4} className = "bg-light" >
+
+          <InputGroup className="mb-3" className="mt-3 mb-3">
+            <FormControl
+              placeholder="Search User"
+              aria-label="Search User Here"
+              aria-describedby="basic-addon2"
+            />
+            <InputGroup.Append>
+              <Button variant="outline-secondary">Button</Button>
+            </InputGroup.Append>
+          </InputGroup>
+          
+        <ChatHead />
+        <ChatHead />
+        <ChatHead />
+
+        </Col>
+
+        <Col md={8}>
+          <HeadSection />
+          <Chat />
+        </Col>
+      </Row>
+
+    </Container>
   );
 };
 
