7a367eb2a47ee91b372548f7da3ecd7b803fb6e8
Gunjan Kalita <gunjankalita836@gmail.com>
2020-09-19 22:33:27 +0530

    Add firebase firestore

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   Container,
   Row,
@@ -11,32 +11,43 @@ import ChatHead from '../Components/ChatHead/ChatHead';
 import HeadSection from '../Components/Chat/HeadSection';
 import Chat from '../Components/Chat/Chat';
 import TypeArea from '../Components/TypeArea';
+import CurrentUser from '../Components/CurrentUser';
+
+import { firestore } from '../firebase';
 
 const Dashboard = () => {
+  const [chatUser, setChatUser] = useState({});
   const [state, setState] = useState({
-    users: [
-      {
-        id: '0',
-        name: 'Gunjan Kalita',
-        email: 'gunjan@gmail.com',
-      },
-      {
-        id: '1',
-        name: 'Other One Ka',
-        email: 'okfine@gmail.com',
-      },
-      {
-        id: '2',
-        name: 'Sara Du',
-        email: 'sarea@gmail.com',
-      },
-      {
-        id: '3',
-        name: 'Sameer Sudhi',
-        email: 'sameer@gmail.com',
-      },
-    ],
+    users: [],
   });
+
+  useEffect(() => {
+    firestore
+      .collection('users')
+      .get()
+      .then((snapshot) => {
+        const users = [];
+        snapshot.docs.map((doc) => {
+          const user = {
+            id: doc.id,
+            ...doc.data(),
+          };
+          users.push(user);
+        });
+        setState({ ...state, users });
+      })
+      .catch((err) => {
+        console.log('err');
+        console.log(err);
+      });
+  });
+
+  const onUserClicked = (user) => {
+    setChatUser((prevState) => {
+      return { ...user };
+    });
+    console.log(chatUser);
+  };
 
   return (
     <Container
@@ -52,7 +63,8 @@ const Dashboard = () => {
       }}
     >
       <Row>
-        <Col md={4} className="bg-light" style={{ marginTop: '8%' }}>
+        <Col md={4} className="bg-light" style={{ marginTop: '1%' }}>
+          <CurrentUser />
           <InputGroup className="mb-3" className="mt-3 mb-3">
             <FormControl
               placeholder="Search User"
@@ -65,12 +77,12 @@ const Dashboard = () => {
           </InputGroup>
 
           {state.users.map((user) => (
-            <ChatHead user={user} />
+            <ChatHead user={user} key={user.id} onClick={onUserClicked} />
           ))}
         </Col>
 
         <Col md={8}>
-          <HeadSection />
+          <HeadSection user={chatUser} />
           <Chat />
           <TypeArea />
         </Col>
